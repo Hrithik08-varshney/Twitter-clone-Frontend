df84a5a9fac694e528c1c8191172b759da5b294e
Hrithik Varshney <hritik.varshney2000@gmail.com>
2025-05-02 17:43:17 +0530

    routing setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      
-    </>
-  );
+		<div className='flex max-w-6xl mx-auto'>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
